f48910f3269ed543dec8d8a552d5f8fccd88e6c0
EC2 Default User <ec2-user@ip-172-31-45-103.ap-northeast-1.compute.internal>
2024-08-04 08:51:34 +0000

    Add edit file

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
   
+  
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
