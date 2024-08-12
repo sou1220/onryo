1e7e7b2112b1d66c147c28d662bb3cf3cef06985
EC2 Default User <ec2-user@ip-172-31-45-103.ap-northeast-1.compute.internal>
2024-08-12 11:53:42 +0000

    Edit search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,9 +3,9 @@ class SearchesController < ApplicationController
   def search
     @model = params[:model]
     @word = params[:word]
-    if @model == "user"
+    if @model == "User"
       @users = User.looks(params[:search], params[:word])
-    else
+    elsif @model == "Post"
       @posts = Post.looks(params[:search], params[:word])
     end
   end
