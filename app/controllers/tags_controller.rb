5e59d0acbb7b8d7cf375014c35432ee813b7f61c
EC2 Default User <ec2-user@ip-172-31-45-103.ap-northeast-1.compute.internal>
2024-09-05 09:28:48 +0000

    Edit tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,16 @@
 class TagsController < ApplicationController
   def tag_search
-    @model = Post
-    @word = params[:word]
+  @model = Post
+  @word = params[:word]
+  @search_word = params[:search_word]
+
+  if @word.present?
     @posts = Post.where(tag: @word)
-    render "tag_search"
+  elsif @search_word.present?
+    @posts = Post.where("tag LIKE ?", "%#{@search_word}%")
+  else
+    @posts = Post.none
+  end
+  render "tag_search"
   end
 end
