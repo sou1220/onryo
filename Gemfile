5828a0b383953035c075959ba21b63922fc5ce53
EC2 Default User <ec2-user@ip-172-31-45-103.ap-northeast-1.compute.internal>
2024-11-02 12:21:09 +0000

    Add kaminari

--- Gemfile
+++ Gemfile
@@ -68,4 +68,5 @@ gem "net-pop"
 gem "net-imap"
 
 gem 'rails-i18n'
+gem 'kaminari'
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    @posts = Post.all.page(params[:page])
   end
 
   def show
